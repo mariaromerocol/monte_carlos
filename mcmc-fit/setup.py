6cd1f049f889670f428be17e8340d1be233f4a3b
Isabel Romero <mariaisabel513@gmail.com>
2022-08-31 22:15:34 -0400

    folders with order

---
+++ mcmc-fit/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='markov chain monte carlo method for fits',
+    author='mariaromero',
+    license='MIT',
+)
